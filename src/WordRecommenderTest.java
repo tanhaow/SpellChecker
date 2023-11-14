1f2f2d78c599909c8f885459c7744bce6a5d39a7
Lisa Ling <lisaling.yx@gmail.com>
2023-11-13 20:25:32 -0500

    Added test for getValidCandidates method in WordRecommender class

--- src/WordRecommenderTest.java
+++ src/WordRecommenderTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WordRecommenderTest {
@@ -41,6 +43,17 @@ public class WordRecommenderTest {
         double expect = 2.5;
         assertEquals(expect,similarity, 0.01);
     }
+
+    @Test
+    void testGetValidCandidates() {
+        HashMap<Double, ArrayList<String>> candidates = wordRec.getValidCandidates("coonterrevolutionaries",0,0.5);
+        HashMap<Double, ArrayList<String>> expect = new HashMap<Double, ArrayList<String>>();
+        double similarity  = wordRec.getSimilarity("coonterrevolutionaries", "counterrevolutionaries");
+        expect.put(similarity, new ArrayList<>());
+        expect.get(similarity).add("counterrevolutionaries");
+        assertEquals(expect,candidates);
+    }
+
 
     @Test
     void testGetWordSuggestions0() {
